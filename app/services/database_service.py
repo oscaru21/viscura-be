385cc7dcf1e6769bdb2d4258d37b6d90ae24fe9f
Aaron Lou <lou22103@gmail.com>
2024-11-06 22:02:40 -0500

    add update_record() and select_record()

--- app/services/database_service.py
+++ app/services/database_service.py
@@ -30,6 +30,39 @@ class DatabaseService:
             self.cursor.execute(query)
         return self.cursor.fetchall()
     
+    def select_record(self, table, conditions=None):
+        """
+        Select records from the database based on conditions
+        :param table: Name of the table
+        :param conditions: Dictionary with column-value pairs for WHERE clause
+        :return: List of records as dictionaries
+        """
+        query = f"SELECT * FROM {table}"
+        if conditions:
+            query += " WHERE " + ' AND '.join([f"{k}=%s" for k in conditions.keys()])
+            self.cursor.execute(query, list(conditions.values()))
+        else:
+            self.cursor.execute(query)
+        return self.cursor.fetchall()
+    
+    def update_record(self, table, data, conditions):
+        """
+        Update records in the database based on conditions
+        :param table: Name of the table
+        :param data: Dictionary with column-value pairs to update
+        :param conditions: Dictionary with column-value pairs for WHERE clause
+        :return: Number of rows affected
+        """
+        set_clause = ', '.join([f"{k} = %s" for k in data.keys()])
+        condition_clause = ' AND '.join([f"{k} = %s" for k in conditions.keys()])
+        query = f"UPDATE {table} SET {set_clause} WHERE {condition_clause}"
+        
+        values = list(data.values()) + list(conditions.values())
+        self.cursor.execute(query, values)
+        self.connection.commit()
+        
+        return self.cursor.rowcount 
+    
     def delete_record(self, table, conditions):
         query = f"DELETE FROM {table} WHERE " + ' AND '.join([f"{k}=%s" for k in conditions.keys()])
         self.cursor.execute(query, list(conditions.values()))
