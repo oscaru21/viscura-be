67f466be373ae7d75ddeda888761803c339bf321
Aaron Lou <lou22103@gmail.com>
2024-11-15 23:41:32 -0500

    change max length in image description service from 10 to 30 tokens

--- app/services/image_description_service.py
+++ app/services/image_description_service.py
@@ -5,5 +5,5 @@ class  ImageDescriptionService:
     def __init__(self):
         self.caption_generation_model = CaptionGenerationModel()
 
-    def generate_caption(self, embedding, max_length=10):
+    def generate_caption(self, embedding, max_length=30):
         return self.caption_generation_model.evaluate(embedding, max_length)
